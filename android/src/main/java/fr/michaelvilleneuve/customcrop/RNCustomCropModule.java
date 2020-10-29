6aaa7efa936ca013639eee05aafbc38b3bb2df04
Tuan Luong <tuanluong.it@gmail.com>
2020-10-29 13:55:00 +0700

    Init opencv lib

--- android/src/main/java/fr/michaelvilleneuve/customcrop/RNCustomCropModule.java
+++ android/src/main/java/fr/michaelvilleneuve/customcrop/RNCustomCropModule.java
@@ -2,46 +2,36 @@
 package fr.michaelvilleneuve.customcrop;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableMap;
 
+import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
-import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfInt;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
-import org.opencv.imgcodecs.*;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-import org.opencv.calib3d.Calib3d;
-
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 
 public class RNCustomCropModule extends ReactContextBaseJavaModule {
+
+  static {
+    if (!OpenCVLoader.initDebug()) {
+      Log.e("RNCustomCropModule", "Could not init OpenCV");
+    } else {
+      Log.d("RNCustomCropModule", "OpenCV initialized!!!");
+    }
+  }
 
   private final ReactApplicationContext reactContext;
 
